62985e1a8a8a14627c2b9c0057c7f6736413e2a3
Sudin <sudintamang3324@gmail.com>
2025-09-27 19:20:50 +0545

    NumPy basics

---
+++ NumPy/Basics.py
@@ -0,0 +1,30 @@
+import numpy as np 
+import sys
+
+# Inserting varius multidimensional arrays
+a = np.array([[1,2,3],
+             [2,3,4],
+             [5,1,2]]) #3X3 dimension
+b = np.array([[1,2.01,3.44,4,5],[6,7,8,9,10],[11,12,13,14,15], [16,17,18,19,20]]) # 4X5 dimension
+
+#prints the size of array in each dimension
+# for a matrix with n rows and m columns the shape would be (n,m)
+print(a.shape) # dispalyed in cartsian form 
+print(b.shape)
+
+#number of dimensionas in an array
+print(b.ndim)
+print(a.ndim)
+
+#total no of elements in that array
+print(a.size)
+print(b.size)
+
+#the type of element inside of the array
+print(a.dtype)
+print(b.dtype)
+
+#the size of each element in the array 
+#for float64 has itemSize 8 and complez32 has itemSize 4
+print(a.itemsize)
+print(b.itemsize)
---
+++ NumPy/modification.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import sys
+
+# accessing/ changing the specific element, row, columns ets
+
+a = np.array([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]])
+
+print(a[1,3]) # to get a speicific elemen array_name.[Row,column]
